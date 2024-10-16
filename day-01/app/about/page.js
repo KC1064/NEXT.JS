23431c0ec6f7ad2ea3beeebb0ed91d1336e1c6fa
KC1064 <kiron08072003@gmail.com>
2024-10-16 18:55:52 +0530

    Day-01: Routing and Nested Routing

---
+++ day-01/app/about/page.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h1>Hello I am The About page</h1>
+      <div className="w-max gap-4 flex mt-2">
+        <Link href="/">
+          <button className="h-max w-max px-2 py-1 bg-blue-500 rounded-lg">
+            Home
+          </button>
+        </Link>
+        <Link href="about">
+          <button className="h-max w-max px-2 py-1 bg-blue-500 rounded-lg">
+            Login
+          </button>
+        </Link>
+        <Link href="login">
+          <button className="h-max w-max px-2 py-1 bg-blue-500 rounded-lg">
+            About
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
---
+++ day-01/app/not-found.js
@@ -0,0 +1,8 @@
+export default function notFound(){
+    return (
+        <div>
+            404 | Page was Not Found
+        </div>
+
+    )
+}
